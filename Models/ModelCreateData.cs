d2a5b6042d22eae74ad24586a36904b08c7e07ac
Saurer <redmmond@gmail.com>
2020-07-22 07:26:32 +0900

    Replace attribute creation tool with selector when creating a model

--- Models/ModelCreateData.cs
+++ Models/ModelCreateData.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Threading.Tasks;
 using AuroraCore;
 using Parallax.Services;
@@ -12,7 +13,7 @@ namespace Parallax.Models {
     }
 
     public class ModelCreateData {
-        private List<(string, int)> attributes = new List<(string, int)>();
+        private List<AttrData> attributes = new List<AttrData>();
 
         [Required, MinLength(1)]
         public string Name { get; set; }
@@ -23,29 +24,35 @@ namespace Parallax.Models {
         [Required]
         public EventType EventType { get; set; } = EventType.Attribute;
 
-        public IEnumerable<(string, int)> Attributes {
+        public IEnumerable<AttrData> Attributes {
             get {
                 return attributes;
             }
         }
 
-        public void AddAttribute((string, int) attr) {
-            attributes.Add(attr);
+        public void AddAttribute(AttrData value) {
+            attributes.Add(value);
         }
 
-        public void RemoveAttribute((string, int) attr) {
-            attributes.Remove(attr);
+        public void RemoveAttribute(AttrData value) {
+            attributes.Remove(value);
+        }
+
+        public bool HasAttribute(AttrData value) {
+            return attributes.Contains(value);
+        }
+
+        public bool HasAttribute(int id) {
+            return attributes.Any(a => a.ID == id);
         }
 
         public async Task<int> Execute(EngineBase engine, CredentialsService service) {
             // Create model
             int modelID = await service.ProcessEvent(engine, new FederatedEvent((int)EventType, StaticEvent.Model, Parent, Name));
 
-            // Create attributes
-            foreach (var (name, dataType) in attributes) {
-                int attrID = await service.ProcessEvent(engine, new FederatedEvent(StaticEvent.Attribute, StaticEvent.Individual, StaticEvent.AttributeModel, name));
-                await service.ProcessEvent(engine, new FederatedEvent(attrID, StaticEvent.DataType, attrID, dataType.ToString()));
-                await service.ProcessEvent(engine, new FederatedEvent(modelID, StaticEvent.Attribute, modelID, attrID.ToString()));
+            // Attach attributes
+            foreach (var attr in attributes) {
+                await service.ProcessEvent(engine, new FederatedEvent(modelID, StaticEvent.Attribute, modelID, attr.ID.ToString()));
             }
 
             return modelID;

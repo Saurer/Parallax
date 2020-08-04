a5b999b997e060bd57e6e9c5baa0902eaf8e55f5
Saurer <redmmond@gmail.com>
2020-08-04 10:03:09 +0900

    Add cardinality & required to create model dialog

--- Models/AddModelAttributeData.cs
+++ Models/AddModelAttributeData.cs
@@ -3,26 +3,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Parallax.Models {
     public class AddModelAttributeData {
-        private bool cardinalityInfinite = false;
-
         [Required]
         public AttrData Attribute { get; set; }
         public bool Required { get; set; }
 
-        public bool CardinalityInfinite {
-            get {
-                return cardinalityInfinite;
-            }
-            set {
-                if (value) {
-                    Cardinality = 1;
-                }
-
-                cardinalityInfinite = value;
-            }
-        }
-
-        [Range(1, Int32.MaxValue)]
+        [Range(0, Int32.MaxValue)]
         public int Cardinality { get; set; } = 1;
+
+        public bool Valid =>
+            Cardinality >= 0;
     }
 }
--- Models/ModelCreateData.cs
+++ Models/ModelCreateData.cs
@@ -7,7 +7,7 @@ using Parallax.Services;
 
 namespace Parallax.Models {
     public class ModelCreateData {
-        private List<AttrData> attributes = new List<AttrData>();
+        private List<AddModelAttributeData> attributes = new List<AddModelAttributeData>();
 
         [Required, MinLength(1)]
         public string Name { get; set; }
@@ -18,26 +18,23 @@ namespace Parallax.Models {
         [Required]
         public int EventType { get; set; }
 
-        public IEnumerable<AttrData> Attributes {
+        public IEnumerable<AddModelAttributeData> Attributes {
             get {
                 return attributes;
             }
         }
 
-        public void AddAttribute(AttrData value) {
+        public void AddAttribute(AddModelAttributeData value) {
             attributes.Add(value);
         }
 
-        public void RemoveAttribute(AttrData value) {
-            attributes.Remove(value);
-        }
-
-        public bool HasAttribute(AttrData value) {
-            return attributes.Contains(value);
+        public void RemoveAttribute(int id) {
+            var attr = attributes.Where(l => l.Attribute.ID == id).SingleOrDefault();
+            attributes.Remove(attr);
         }
 
         public bool HasAttribute(int id) {
-            return attributes.Any(a => a.ID == id);
+            return attributes.Any(a => a.Attribute.ID == id);
         }
 
         public async Task<int> Execute(EngineBase engine, CredentialsService service) {
@@ -46,7 +43,25 @@ namespace Parallax.Models {
 
             // Attach attributes
             foreach (var attr in attributes) {
-                await service.ProcessEvent(engine, new FederatedEvent(modelID, StaticEvent.Attribute, modelID, attr.ID.ToString()));
+                var eventID = await service.ProcessEvent(engine, new FederatedEvent(modelID, StaticEvent.Attribute, modelID, attr.Attribute.ID.ToString()));
+
+                if ((Const.DefaultRequired == 1) != attr.Required) {
+                    await service.ProcessEvent(engine, new FederatedEvent(
+                        eventID,
+                        StaticEvent.Required,
+                        attr.Attribute.ID,
+                        attr.Required ? "1" : "0"
+                    ));
+                }
+
+                if (Const.DefaultCardinality != attr.Cardinality) {
+                    await service.ProcessEvent(engine, new FederatedEvent(
+                        eventID,
+                        StaticEvent.Cardinality,
+                        attr.Attribute.ID,
+                        attr.Cardinality.ToString()
+                    ));
+                }
             }
 
             return modelID;
